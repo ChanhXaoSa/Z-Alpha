d9d2c6eff7d18c05599e023b1f5f3845c437bc2f
chandinh191 <vinhtc191@gmail.com>
2023-11-30 11:11:01 +0700

    .

--- src/WebUI/ConfigureServices.cs
+++ src/WebUI/ConfigureServices.cs
@@ -25,36 +25,36 @@ public static class ConfigureServices
 
         services.AddRazorPages();
 
-        //services.AddScoped<FluentValidationSchemaProcessor>(provider =>
-        //{
-        //    var validationRules = provider.GetService<IEnumerable<FluentValidationRule>>();
-        //    var loggerFactory = provider.GetService<ILoggerFactory>();
+      /*  services.AddScoped<FluentValidationSchemaProcessor>(provider =>
+        {
+            var validationRules = provider.GetService<IEnumerable<FluentValidationRule>>();
+            var loggerFactory = provider.GetService<ILoggerFactory>();
 
-        //    return new FluentValidationSchemaProcessor(provider, validationRules, loggerFactory);
-        //});
+            return new FluentValidationSchemaProcessor(provider, validationRules, loggerFactory);
+        });*/
 
-        // Customise default API behaviour
+        //Customise default API behaviour
         services.Configure<ApiBehaviorOptions>(options =>
             options.SuppressModelStateInvalidFilter = true);
 
-        //services.AddOpenApiDocument((configure, serviceProvider) =>
-        //{
-        //    var fluentValidationSchemaProcessor = serviceProvider.CreateScope().ServiceProvider.GetRequiredService<FluentValidationSchemaProcessor>();
+      /*  services.AddOpenApiDocument((configure, serviceProvider) =>
+        {
+            var fluentValidationSchemaProcessor = serviceProvider.CreateScope().ServiceProvider.GetRequiredService<FluentValidationSchemaProcessor>();
 
-        //    // Add the fluent validations schema processor
-        //    configure.SchemaProcessors.Add(fluentValidationSchemaProcessor);
+            // Add the fluent validations schema processor
+            configure.SchemaProcessors.Add(fluentValidationSchemaProcessor);
 
-        //    configure.Title = "CleanArchitecture API";
-        //    configure.AddSecurity("JWT", Enumerable.Empty<string>(), new OpenApiSecurityScheme
-        //    {
-        //        Type = OpenApiSecuritySchemeType.ApiKey,
-        //        Name = "Authorization",
-        //        In = OpenApiSecurityApiKeyLocation.Header,
-        //        Description = "Type into the textbox: Bearer {your JWT token}."
-        //    });
+            configure.Title = "CleanArchitecture API";
+            configure.AddSecurity("JWT", Enumerable.Empty<string>(), new OpenApiSecurityScheme
+            {
+                Type = OpenApiSecuritySchemeType.ApiKey,
+                Name = "Authorization",
+                In = OpenApiSecurityApiKeyLocation.Header,
+                Description = "Type into the textbox: Bearer {your JWT token}."
+            });
 
-        //    configure.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("JWT"));
-        //});
+            configure.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("JWT"));
+        });*/
 
         return services;
     }
