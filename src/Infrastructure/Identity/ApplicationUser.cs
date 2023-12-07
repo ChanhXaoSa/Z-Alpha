0d55efee19d116fa3183d2970bfe47d3f5988853
chandinh191 <vinhtc191@gmail.com>
2023-12-07 11:08:29 +0700

    fix

--- src/Infrastructure/Identity/ApplicationUser.cs
+++ src/Infrastructure/Identity/ApplicationUser.cs
@@ -6,18 +6,5 @@ namespace CleanArchitecture.Infrastructure.Identity;
 
 public class ApplicationUser : IdentityUser
 {
-    //public string UserName { get; set; } = null!;
-    //public string LastName { get; set; } = null!;
-    public string? FullName { get; set; }
-    public DateTime BirthDay { get; set; }
-    public string? Address { get; set; }
-    public string? Phone { get; set; }
-    public PremiumStatus IsPremium { get; set; }
-    public string? Avatar { get; set; }
-    public string? AvatarUrl { get; set; }
-    public UserStatus Status { get; set; }
-
-    public Guid? ManagerAccountId { get; set; }
-    public virtual ManagerAccount? ManagerAccount { get; set; }
-    public IList<Transaction>? Transactions { get; private set; }
+   
 }
