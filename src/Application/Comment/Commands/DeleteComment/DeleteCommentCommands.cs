37b648ae444e19f868b9164fb5a43d96677dc1b9
Xe tăng tông cổng <108561127+LETUANKIET29@users.noreply.github.com>
2023-12-13 12:26:05 +0700

    Add CRUD file

---
+++ src/Application/Comment/Commands/DeleteComment/DeleteCommentCommands.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Comment.Commands.DeleteComment;
+internal class DeleteCommentCommands
+{
+}
