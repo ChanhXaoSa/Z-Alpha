74f3587d1078960cbfab7e00efa186b1f8121f61
Xe tăng tông cổng <108561127+LETUANKIET29@users.noreply.github.com>
2023-12-29 11:48:09 +0700

    Update User Info

---
+++ src/WebUI/Controllers/MVC/SpychologistController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers.MVC;
+public class SpychologistController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Info()
+    {
+        return View();
+    }
+}
