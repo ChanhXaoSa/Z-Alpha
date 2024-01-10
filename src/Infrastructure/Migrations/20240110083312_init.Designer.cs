8e56bdfaa2ef1f942d87f6660d71d598beafd244
chanhday <trieudoublenguyen@gmail.com>
2024-01-10 15:39:15 +0700

    update db

--- src/Infrastructure/Migrations/20240110083312_init.Designer.cs
+++ src/Infrastructure/Migrations/20240110083312_init.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanArchitecture.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20231226064314_init")]
+    [Migration("20240110083312_init")]
     partial class init
     {
         /// <inheritdoc />
