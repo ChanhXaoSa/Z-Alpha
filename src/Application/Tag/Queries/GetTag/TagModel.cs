21176901a8dd283d0b707bb5fcd7b36b4f9dc592
chandinh191 <vinhtc191@gmail.com>
2024-01-14 16:04:09 +0700

    banner + name + tag

---
+++ src/Application/Tag/Queries/GetTag/TagModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Common.Mappings;
+using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Enums;
+
+namespace CleanArchitecture.Application.Tag.Queries.GetTag;
+public class TagModel : IMapFrom<Domain.Entities.Tag>
+{
+    public string TagName { get; set; }
+    public virtual IList<Domain.Entities.PostTag>? PostTags { get; set; }
+}
