c2ee9f1e17a503daf01d380128a3f6f7bf7fb975
chanhday <trieudoublenguyen@gmail.com>
2024-01-15 02:50:50 +0700

    update authen layout

--- src/WebUI/Controllers/MVC/LoginController.cs
+++ src/WebUI/Controllers/MVC/LoginController.cs
@@ -1,8 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using CleanArchitecture.Domain.Identity;
 
 namespace WebUI.Controllers.MVC;
 
+[Authorize]
+[Route("/Home")]
 public class LoginController : ControllerBaseMVC
 {
+    private readonly UserManager<UserAccount> _userManager;
+    private readonly SignInManager<UserAccount> _signInManager;
+
+    public LoginController(UserManager<UserAccount> userManager, SignInManager<UserAccount> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
     public IActionResult index() { return View(); }
+
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [AllowAnonymous]
+    [HttpGet("logout")]
+    public async Task<IActionResult> Logout([FromQuery] string redirectUrl)
+    {
+        // Clear the existing external cookie
+        await HttpContext.SignOutAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme);
+
+        if (!string.IsNullOrEmpty(redirectUrl))
+        {
+            return LocalRedirect("/Home");
+        }
+
+        return LocalRedirect(redirectUrl);
+    }
 }
