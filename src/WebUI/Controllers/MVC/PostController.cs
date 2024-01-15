ef5396d96dcb487c008bf9f7a9c8e5a977825c15
chandinh191 <vinhtc191@gmail.com>
2024-01-16 02:58:11 +0700

    post detail

---
+++ src/WebUI/Controllers/MVC/PostController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers.MVC;
+public class PostController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
