9d916e708df0b5db1587d6053a7194566ac15166
chandinh191 <vinhtc191@gmail.com>
2024-01-20 02:42:27 +0700

    xóa lộn

---
+++ src/WebUI/Controllers/MVC/NewPostController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers.MVC;
+public class NewPostController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
