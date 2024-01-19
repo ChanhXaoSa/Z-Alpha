a0be2b848c3f5d20acceb30255b0b87e13731376
chandinh191 <vinhtc191@gmail.com>
2024-01-20 02:38:50 +0700

    customer profile

---
+++ src/WebUI/Controllers/MVC/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ZAlpha.Application.Common.Interfaces;
+using ZAlpha.Domain.Identity;
+
+namespace WebUI.Controllers.MVC;
+public class CustomerController : ControllerBaseMVC
+{
+    private readonly IIdentityService _identityService;
+    private readonly UserManager<UserAccount> _userManager;
+    private readonly SignInManager<UserAccount> _signInManager;
+
+    public CustomerController(IIdentityService identityService, UserManager<UserAccount> userManager, SignInManager<UserAccount> signInManager)
+    {
+        _identityService = identityService;
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
