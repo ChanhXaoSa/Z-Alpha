40c090e27e0fc9a9c5de2ca566bf1352442bf0f3
chanhday <trieudoublenguyen@gmail.com>
2024-01-19 15:57:04 +0700

    update login session

--- src/WebUI/Controllers/MVC/PostController.cs
+++ src/WebUI/Controllers/MVC/PostController.cs
@@ -6,27 +6,37 @@ using ZAlpha.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using ZAlpha.Application.Comment.Queries.GetCommentById;
 using ZAlpha.Application.Comment.Commands.CreateComment;
+using ZAlpha.Application.Common.Interfaces;
 
 namespace WebUI.Controllers.MVC;
 public class PostController : ControllerBaseMVC
 {
 
-   /* public async Task<IActionResult> Index(Guid postID)
-    {
-        var post = await Mediator.Send(new GetPostByIdQueries() { Id = postID });
-        var listComment = await Mediator.Send(new GetCommentByPostIdQueries() { PostId = postID, Page = 1, Size = 100 });
-        ViewBag.listComment = listComment;
-        return View(post);
-    }*/
+    /* public async Task<IActionResult> Index(Guid postID)
+     {
+         var post = await Mediator.Send(new GetPostByIdQueries() { Id = postID });
+         var listComment = await Mediator.Send(new GetCommentByPostIdQueries() { PostId = postID, Page = 1, Size = 100 });
+         ViewBag.listComment = listComment;
+         return View(post);
+     }*/
+    private readonly IIdentityService _identityService;
 
+    public PostController(IIdentityService identityService)
+    {
+        _identityService = identityService;
+    }
 
     public async Task<IActionResult> Index(string userAccountId, Guid postId, string description)
     {
-        if (userAccountId != null && description != null)
+        
+        var user = await _identityService.GetUserByNameAsync(User.Identity.Name);
+        if (User.Identity.IsAuthenticated && description != null)
         {
-            var commentId = Mediator.Send(new CreateCommentCommands() { UserAccountId = userAccountId, PostId = postId, Description = description }).Result;
+            var commentId = Mediator.Send(new CreateCommentCommands() { UserAccountId = user.Id, PostId = postId, Description = description }).Result;
+        } else if (!User.Identity.IsAuthenticated && description != null)
+        {
+            return Redirect("~/Login");
         }
-
         var post = await Mediator.Send(new GetPostByIdQueries() { Id = postId });
         var listComment = await Mediator.Send(new GetCommentByPostIdQueries() { PostId = postId, Page = 1, Size = 100 });
         ViewBag.listComment = listComment;
