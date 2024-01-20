1dcb1b12ce7f8dc4e16bb1d839bb5889c250e2fd
Xe tăng tông cổng <108561127+LETUANKIET29@users.noreply.github.com>
2024-01-20 20:53:39 +0700

    Update Newpost Psychologist

--- src/Application/Tag/Queries/GetAllTag/GetAllTagQueries.cs
+++ src/Application/Tag/Queries/GetAllTag/GetAllTagQueries.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MediatR;
+using ZAlpha.Application.Common.Models;
 
 namespace ZAlpha.Application.Tag.Queries.GetTag;
-internal class GetTagQueries
+public class GetAllTagQueries : IRequest<PaginatedList<TagModel>>
 {
+
 }
