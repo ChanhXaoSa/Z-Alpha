e83d101e5fbbf021c81d658ba61370ceb52e92b0
Xe tăng tông cổng <108561127+LETUANKIET29@users.noreply.github.com>
2024-01-23 14:34:16 +0700

    Add Admin Page

---
+++ src/WebUI/Controllers/MVC/Admin/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers.MVC.Admin;
+public class AdminController : ControllerBaseMVC
+{
+    public IActionResult Index()
+    {
+        return View("./AdminHome/Index");
+    }
+
+    public IActionResult index2()
+    {
+        return View("./AdminHome/index2");
+    }
+}
